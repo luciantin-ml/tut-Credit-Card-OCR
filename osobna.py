c5d9209723e24685c25563e7cdb18d091a0e16f6
luciantin <lord66tin66@gmail.com>
2021-03-04 11:20:01 +0100

    tjt

--- osobna.py
+++ osobna.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
 import imutils
+# from easyocr import Reader
+# reader = Reader(['en'], -1)
 
 
 def nothing(x):
     frame = cv2.imread('images/osobna1.jpg')
     frame = imutils.resize(frame, width=600)
+    frame = cv2.rotate(frame, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
 
     # get current positions of the trackbars
     ilowH = cv2.getTrackbarPos('lowH', 'image')
@@ -24,7 +27,7 @@ def nothing(x):
     # Apply the mask on the image to extract the original color
     frame = cv2.bitwise_and(frame, frame, mask=mask)
     cv2.imshow('image', frame)
-
+    # print(reader.readtext(frame))
 
 # Open the camera
 
@@ -33,15 +36,15 @@ cv2.namedWindow('image')
 
 # create trackbars for color change
 cv2.createTrackbar('lowH', 'image', 0, 179, nothing)
-cv2.createTrackbar('highH', 'image', 169, 179, nothing)
+cv2.createTrackbar('highH', 'image', 179, 179, nothing)
 
-cv2.createTrackbar('lowS', 'image', 1, 255, nothing)
-cv2.createTrackbar('highS', 'image', 18, 255, nothing)
+cv2.createTrackbar('lowS', 'image', 0, 255, nothing)
+cv2.createTrackbar('highS', 'image', 255, 255, nothing)
 
-cv2.createTrackbar('lowV', 'image', 200, 255, nothing)
-cv2.createTrackbar('highV', 'image', 255, 255, nothing)
+cv2.createTrackbar('lowV', 'image', 0, 255, nothing)
+cv2.createTrackbar('highV', 'image', 80, 255, nothing)
 
-
+nothing('asd')
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
